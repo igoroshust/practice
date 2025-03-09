d1a0f9edba095c72604e5f456de38b5b38c6c603
Igor <igoroshust75@gmail.com>
2025-03-09 17:26:17 +0900

    feat: sending message (emailmultialternatives)

--- attainments/sending_letters/docs.py
+++ attainments/sending_letters/docs.py
@@ -32,19 +32,51 @@
 # Шаг 4. Отпределиться, где и какой код написать для отправки письма новому пользователю.
 # Переходим в приложение (accounts), создаём файл forms.py
 
+# from allauth.account.forms import SignupForm
+# from django.core.mail import send_mail
+#
+# class CustomSignupForm(SignupForm):
+#     def save(self, request):
+#         user = super().save(request)
+#
+#         # send_mail - отправка письма указанному получателю в recipient_list
+#         send_mail(
+#             subject='Добро пожаловать на сайт!', # тема письма
+#             message=f'{user.username}, вы успешно зарегистрировались!', # текстовое сообщение
+#             from_email = None, # будет использовано значение DEFAULT_FROM_EMAIL
+#             recipient_list=[user.email] # список получателей.
+#         )
+#         return user
+
+
+"""HTML-письма"""
+# Чтобы отправить HTML по почте, нужно воспользоваться EmailMultiAlternatives.
+# Он позволяет одновременно отправить текстовое сообщение и приложить к нему версию c HTML-разметкой.
+# Так, почтовые клиенты способные отображать HTML, будут его показывать, а для остальных будет в приоритете текстовая версия.
+
+# EmailMultiAlternatives - класс из модуля 'django.core.mail', который позволяет отправлять электронные письма с несколькими альтернативными
+# форматами содержимого, такими как текст и HTML. Это полезно, когда мы хотим по-разному отображать оформление письма в зависимостей от возможностей почтового клиента получателя.
+
+# # accounts/forms
 from allauth.account.forms import SignupForm
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
 
 class CustomSignupForm(SignupForm):
     def save(self, request):
         user = super().save(request)
 
-        # send_mail - отправка письма указанному получателю в recipient_list
-        send_mail(
-            subject='Добро пожаловать на сайт!', # тема письма
-            message=f'{user.username}, вы успешно зарегистрировались!', # текстовое сообщение
-            from_email = None, # будет использовано значение DEFAULT_FROM_EMAIL
-            recipient_list=[user.email] # список получателей.
+        subject = 'Добро пожаловать на наш сайт!'
+        text = f'{user.username}, вы успешно зарегистрировались на сайте!'
+        html = (
+            f'<b>{user.username}</b>, вы успешно зарегистрировались на сайте '
+            f'<a href="http://127.0.0.1:8000/main">сайте</a>!'
         )
-        return user
+        msg = EmailMultiAlternatives(
+            subject=subject, body=text, from_email=None, to=[user.email]
+        )
+        # метод для добавления альтернативного формата содержимого к письму
+        # attach_alternative(html-код, "text/html")
+        msg.attach_alternative(html, "text/html")
+        msg.send()
 
+        return user
--- dj_project/accounts/forms.py
+++ dj_project/accounts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from allauth.account.forms import SignupForm
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMultiAlternatives
 from django.contrib.auth.models import User
 
 
@@ -21,14 +21,80 @@ class SignUpForm(UserCreationForm):
             "password2",
         )
 
-    def save(self, commit=True): # если commit=True, мы отправляем письмо на адрес ЭП пользователя с помощью функции send_mail
-        user = super().save(commit=commit) # commit позволяет контролировать, сохраняется ли пользователь в БД сразу или мы можем сделать это позже.
+    # def save(self, commit=True): # если commit=True, мы отправляем письмо на адрес ЭП пользователя с помощью функции send_mail
+    #     user = super().save(commit=commit) # commit позволяет контролировать, сохраняется ли пользователь в БД сразу или мы можем сделать это позже.
+    #     if commit:
+    #         # Отправка письма о регистрации
+    #         send_mail(
+    #             subject='Добро пожаловать',
+    #             message='Вы успешно зарегистрировались',
+    #             from_email='igoroshust@yandex.ru',
+    #             recipient_list=[user.email],
+    #         )
+    #     return user
+
+    # def save(self, commit=True):
+    #     user = super().save(commit=commit)
+    #
+    #     if commit:
+    #
+    #         subject = 'Добро пожаловать на наш сайт!'
+    #         text = f'{user.username}, вы успешно зарегистрировались на сайте!'
+    #         html = (
+    #             f'<b>{user.username}</b>, вы успешно зарегистрировались на сайте '
+    #             f'<a href="http://127.0.0.1:8000/main">сайте</a>!'
+    #         )
+    #         msg = EmailMultiAlternatives(
+    #             subject=subject, body=text, from_email='igoroshust@yandex.ru', to=[user.email]
+    #         )
+    #         # метод для добавления альтернативного формата содержимого к письму
+    #         # attach_alternative(html-код, "text/html")
+    #         msg.attach_alternative(html, "text/html")
+    #         msg.send()
+    #
+    #     return user
+
+
+    def save(self, commit=True):
+        user = super().save(commit=commit)
+
         if commit:
-            # Отправка письма о регистрации
-            send_mail(
-                subject='Добро пожаловать',
-                message='Вы успешно зарегистрировались',
-                from_email='igoroshust@yandex.ru',
-                recipient_list=[user.email],
+            subject = 'Добро пожаловать...'
+            text = 'Вы успешно зарегистрировались!'
+            html = (
+                f'<b>{user.username}</b>, Вы успешно зарегистрировались на '
+                f'<a href="http://127.0.0.1:8000/main">сайте</a>'
             )
+
+            msg = EmailMultiAlternatives(
+                subject=subject, body=text, from_email='igoroshust@yandex.ru', to=[user.email]
+            )
+
+            msg.attach_alternative(html, "text/html")
+            msg.send()
+
         return user
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
