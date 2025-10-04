a07e3ae36e1bb51cb688edbefe3b25e9b96ad02a
unknown <igoroshust@yandex.ru>
2025-10-04 20:02:57 +0900

    feat: 01:33:33

--- Python_full_course_bogdan/main.py
+++ Python_full_course_bogdan/main.py
@@ -1,3 +1,7 @@
-name = input("Enter your name: ")
+# name, surname, city = input("Enter your name: "),  input("Enter your surname: "),  input("Enter your city: ")
+# print(name, surname, city)
 
-print(name)
+text = 'tutorial'
+table = str.maketrans('aeiou', '43105')
+result = text.translate(table)
+print(result)
