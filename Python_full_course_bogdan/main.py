8ee73cd6615eb6f361268cd93c16b3b04064aa7c
unknown <igoroshust@yandex.ru>
2025-10-07 19:29:45 +0900

    feat: 04:12:32

--- Python_full_course_bogdan/main.py
+++ Python_full_course_bogdan/main.py
@@ -1,14 +1,2 @@
-# complex_a = 3 + 5j  # 3 - действительная часть, 5 - мнимая + j
-# complex_b = 4 + 7j
+print((10.5).as_integer_ratio())
 
-# complex_a = 10 + 7j  # 3 - действительная часть, 5 - мнимая + j
-# complex_b = 3 + 3j
-
-complex_a = 3 + 5j  # 3 - действительная часть, 5 - мнимая + j
-complex_b = 4 + 7j
-
-div = complex_a / complex_b
-
-print(
-    div,
-)
