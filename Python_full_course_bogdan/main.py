f1f5679b295757977530ec9321ae11100b6831dc
unknown <igoroshust@yandex.ru>
2025-10-08 18:32:23 +0900

    feat: 05:09:37

--- Python_full_course_bogdan/main.py
+++ Python_full_course_bogdan/main.py
@@ -1,2 +1,5 @@
-print((10.5).as_integer_ratio())
+my_nums = [10, 50, 0, 5, 5, 100]
+my_nums.append('123')  # [10, 50, 0, 5, 5, 100, '123']
+my_nums.extend('123')  # [10, 50, 0, 5, 5, 100, '1', '2', '3']
 
+print(my_nums)
