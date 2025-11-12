19f55084d2b9edaf9696810a754b1a3b659821f6
unknown <igoroshust@yandex.ru>
2025-11-12 13:02:43 +0900

    webcademy: feat accordion 46:00

--- webcademy/basic-js/show-hide-block/js/01-show-hide-block.js
+++ webcademy/basic-js/show-hide-block/js/01-show-hide-block.js
@@ -1,10 +1,35 @@
 const button = document.querySelector('#button');
 const content = document.querySelector('#content');
 
-const openText = button.dataset.openText;
-const closeText = button.dataset.closeText;
 
-button.onclick = () => {
+/* Решение учителя */
+button.addEventListener('click', function(){
+    console.log('Click!');
+    
+    if (content.classList.toggle('content-hidden')) {
+        // content.classList.toggle выполнится в любом случае, а условие также выполняется, потому что toggle возвращает true или false
+        button.textContent = "Открыть блок";
+    } else {
+        button.textContent = "Закрыть блок";
+    }
+
+    /* Пошаговое решение 
     content.classList.toggle('content-hidden');
-    button.innerText = content.classList.contains('content-hidden') ? openText : closeText;
-};
+
+    if (content.classList.contains('content-hidden')){
+        button.innerText = "Открыть блок";
+    } else {
+        button.innerText = "Закрыть блок";
+    }
+    */
+});
+
+
+/* Моё решение */
+// const openText = button.dataset.openText;
+// const closeText = button.dataset.closeText;
+
+// button.onclick = () => {
+//     content.classList.toggle('content-hidden');
+//     button.innerText = content.classList.contains('content-hidden') ? openText : closeText;
+// };
---
+++ webcademy/basic-js/show-hide-block/js/02-accordeon.js
@@ -0,0 +1,31 @@
+const headers = document.querySelectorAll("[data-name='accordeon-title']");
+
+/* Итоговый вариант */
+headers.forEach((item) => {
+    item.addEventListener('click', function(){
+        // this содержит информацию о элементе, на который мы кликнули (ссылкается на заголовок, по которому пришёлся клик)
+        // console.log(this);
+        // console.log(this.nextElementSibling);
+        this.nextElementSibling.classList.toggle('content-hidden');
+    });
+});
+
+/* Вариант с отдельной функцией */
+headers.forEach(function(item) {
+    item.addEventListener('click', showContent); // showContent без вызова, т.к. внутри addEventListener передаёт ссылку на функцию как callback - она будет вызвана браузером при клике по умолчанию.
+});
+
+function showContent() {
+    this.nextElementSibling.classList.toggle('content-hidden');
+};
+
+
+
+/* Моё решение */
+// const accordeonTitle = document.querySelectorAll('.list-group-item.list-header');
+
+// accordeonTitle.forEach((item, index) => {
+//     accordeonTitle[index].onclick = () => {
+//         accordeonTitle[index].nextElementSibling.classList.toggle('content-hidden');
+//     }
+// });
